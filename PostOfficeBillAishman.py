233fdc15dad5b1dde9f37f972d8aca6145d20b1a
billaishman <baishman27@gcds.net>
2024-09-10 12:02:08 -0400

    Add files via upload

---
+++ PostOfficeBillAishman.py
@@ -0,0 +1,19 @@
+#Name: Bill Aishman
+#Description: 
+#Bugs:
+#Features:
+#Log
+
+
+def main():
+    data = input("Enter the data: ")
+    dimensions = data.split(",")
+
+    height = float(dimensions[0])
+    length = float(dimensions[1])
+    thickness = float(dimensions[2])
+    while True:
+        if height >= 3.25 and height <= 4.25:
+  
+
+main()
